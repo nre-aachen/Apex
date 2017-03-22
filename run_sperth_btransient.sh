e1e6ccb1760831f039e81d494f72e7e0929476f0
Guilherme Weishar <guilherme@170-188.eduroam.rwth-aachen.de>
2017-03-22 13:31:05 +0100

    Corrected a small mistake in the bash script

--- run_sperth_btransient.sh
+++ run_sperth_btransient.sh
@@ -19,9 +19,6 @@
 ### Request memory you need for your job in TOTAL in MB
 #BSUB -M 18192
 
-### Request the number of compute slots you want to use
-#BSUB -n 12
-
 ### Use esub for Open MPI
 #BSUB -a openmpi
 
